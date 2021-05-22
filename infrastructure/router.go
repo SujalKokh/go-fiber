4510c7c0ca9567ede70d027f3f552bafbc6580ef
Suz'l Shrestha <sujal.kokh@gmail.com>
2021-05-22 14:23:33 +0545

    feat: fiber router

---
+++ infrastructure/router.go
@@ -0,0 +1,37 @@
+package infrastructure
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+// Router -> Fiber Router
+type Router struct {
+	*fiber.App
+}
+
+//NewRouter : all the routes are defined here
+func NewRouter(env Env) Router {
+
+	httpRouter := fiber.New()
+
+	httpRouter.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOrigins:     "http://localhost",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:     "",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}))
+
+	httpRouter.Get("/health-check", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Fiber API up and running ♾️",
+		})
+	})
+
+	return Router{
+		httpRouter,
+	}
+}
