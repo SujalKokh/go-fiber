58b910a6ba3ef7f0530e304ace6d889b4bf525f9
Suz'l Shrestha <sujal.kokh@gmail.com>
2021-05-23 17:15:07 +0545

    feat: user routes added

---
+++ api/routes/user_routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"go-fiber/api/controllers"
+	"go-fiber/infrastructure"
+)
+
+// UserRoute -> structure for user routes
+type UsersRoute struct {
+	controller controllers.UsersController
+	router     infrastructure.Router
+}
+
+// NewUserRoute -> constructor function for user routes
+func NewUsersRoute(
+	controller controllers.UsersController,
+	router infrastructure.Router,
+) UsersRoute {
+	return UsersRoute{
+		controller: controller,
+		router:     router,
+	}
+}
+
+// Setup -> setups the routes
+func (u UsersRoute) Setup() {
+	users := u.router.Group("/users")
+	users.Get("/", u.controller.HandleGetAllUsers)
+}
