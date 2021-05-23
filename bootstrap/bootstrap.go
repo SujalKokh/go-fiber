43ac6c1b671b04bf53ba26f9eb10baedce30a575
Suz'l Shrestha <sujal.kokh@gmail.com>
2021-05-23 17:15:53 +0545

    feat: integrating routes for bootstrap

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -2,6 +2,8 @@ package bootstrap
 
 import (
 	"context"
+	"go-fiber/api/controllers"
+	"go-fiber/api/routes"
 	"go-fiber/infrastructure"
 
 	"go.uber.org/fx"
@@ -10,22 +12,26 @@ import (
 // Modules exported for initilization of the application
 var Module = fx.Options(
 	infrastructure.Module,
+	routes.Module,
+	controllers.Module,
 	fx.Invoke(bootstrap),
 )
 
 func bootstrap(
 	lifecycle fx.Lifecycle,
-	logger infrastructure.Logger,
 	handler infrastructure.Router,
+	routes routes.Routes,
+	logger infrastructure.Logger,
 ) {
 	lifecycle.Append(fx.Hook{
 		OnStart: func(context.Context) error {
 			logger.Zap.Info("Starting the application")
 			logger.Zap.Info("---------------------------")
-			logger.Zap.Info("------- Go-fiber ♾️  -------")
+			logger.Zap.Info("------- Go-fiber 🚀  -------")
 			logger.Zap.Info("---------------------------")
 
 			go func() {
+				routes.Setup()
 				handler.Listen(":5000")
 			}()
 			return nil
