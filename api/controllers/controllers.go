823bf0db826dba5f4be6b39e6f3d8359ffa72ae3
Suz'l Shrestha <sujal.kokh@gmail.com>
2021-05-23 17:15:29 +0545

    feat: controller dependency injection

--- api/controllers/controllers.go
+++ api/controllers/controllers.go
@@ -3,4 +3,6 @@ package controllers
 import "go.uber.org/fx"
 
 // Module exported for initializing application
-var Module = fx.Options()
+var Module = fx.Options(
+	fx.Provide(NewUsersController),
+)
